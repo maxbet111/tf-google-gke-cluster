700a4dc5b6f0a816304120a283db0f0cd9fa583b
maksymkoval1 <maksym.koval@ext.hellofresh.com>
2026-05-19 15:23:28 +0300

    add files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "Регіон Google Cloud (наприклад, us-central1)"
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "ID вашого проєкту в Google Cloud"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  description = "Кількість вузлів у кластері"
+  default     = 2
+}
